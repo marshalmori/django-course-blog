013601d276446f141e95342f14f6d47efc9f5aca
Marshal Mori Cavalheiro <marshal@asimov.marshal>
2024-12-12 22:36:32 +0100

    Create Article database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    pass
+
+
+ARTICLE_STATUS = (
+    ("draft", "draft"),
+    ("inprogress", "in progress"),
+    ("published", "published"),
+)
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default="")
+    word_count = models.IntegerField()
+    twitter_post = models.TextField(blank=True, default="")
+    status = models.CharField(
+        max_length=20,
+        choices=ARTICLE_STATUS,
+        default="draft"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+
