50a9cf327511b2b631a06909385032d1e39ae2a6
Marshal Mori Cavalheiro <marshal@asimov.marshal>
2024-12-14 00:21:13 +0100

    Show all articles in the homepage

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from app.models import Article
 
 
 def home(request):
-    return HttpResponse('Hello World')
+    articles = Article.objects.all()
+    return render(request, "app/home.html", {"articles": articles})
